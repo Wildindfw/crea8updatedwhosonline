db85f0892cab2ec5131c09f8acebe448f379689f
Wildindfw <42122656+Wildindfw@users.noreply.github.com>
2018-12-16 12:57:13 -0600

    Update upgrade.php

--- whosonline2/database/upgrade.php
+++ whosonline2/database/upgrade.php
@@ -1,7 +1,7 @@
 <?php
 
 function whos_online_upgrade_database(){
-    register_site_page('online-members2', array('title' => 'whos_online2::whos_online2', 'column_type' => TWO_COLUMN_RIGHT_LAYOUT), function() {
+    register_site_page('online-members2', array('row' => 'whos_online2::whos_online2', 'column_type' => TWO_COLUMN_RIGHT_LAYOUT), function() {
         Widget::add(null, 'online-members2', 'content', 'middle');
         Widget::add(null, 'online-members2', 'plugin::relationship|friends', 'right');
         Widget::add(null, 'online-members2', 'plugin::relationship|suggestions', 'right');
